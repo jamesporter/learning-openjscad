a47465d7bf995b00405b1786362fc230bd17da94
James Porter <james@amimetic.co.uk>
2018-06-24 13:00:32 +0100

    Create hex-grid.js

---
+++ hex-grid.js
@@ -0,0 +1,43 @@
+// Unlike previous one I think equations for x/y adjustment actually correct in general
+
+function main () {
+    
+  var shapes = [];
+  
+  for(var i=0; i<5; i++){
+      for(var j=0; j<5; j++){
+        shapes.push(hex(1 + Math.random(), i, j));
+      }
+  }
+    
+  return union(
+      shapes
+  )
+}
+
+var pit = Math.PI/3;
+var dv = Math.sin(pit);
+var dh = 1;
+
+function hex(h,x,y) {
+    var dx,dy;
+    if(x % 2 == 0) {
+        dx = x * 1.5 * dh;
+        dy = y * 2 * dv;
+    } else {
+        dx = x * 1.5 * dh ;
+        dy = y * dv * 2 + dv;
+    }
+    return translate([dx, dy, 0],
+        linear_extrude({height: h},
+            polygon([
+                [1,0],
+                [Math.cos(pit),Math.sin(pit)],
+                [-Math.cos(pit),Math.sin(pit)],
+                [-1,0],
+                [-Math.cos(pit),-Math.sin(pit)],
+                [Math.cos(pit),-Math.sin(pit)],
+            ])
+        )
+    )
+}
